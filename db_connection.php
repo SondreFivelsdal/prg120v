d6e59069c8e28aad5399c9dd6b5b5882db257761
sondrefivelsdal <sondrebfivelsdal@gmail.com>
2025-10-23 10:47:30 +0200

    Bruker riktig DB-host (b-studentsql-1.usn.no)

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,11 @@
 <?php
 // db_connection.php
 // ⬇️ Sett inn verdiene fra Dokploy-portalen (ikke localhost hvis DB er egen tjeneste)
-$servername = "mysql";     // f.eks. mysql.dokploy.usn.no eller navnet på db-tjenesten
+$servername = "b-studentsql-1.usn.no";     // f.eks. mysql.dokploy.usn.no eller navnet på db-tjenesten
 $username   = "250158";  
 $password   = "6d61250158";     
-$dbname     = "250158";        
+$dbname     = "250158";
+$port       = 3306;                 
 
 // Koble til MySQL
 $conn = new mysqli($servername, $username, $password, $dbname);
